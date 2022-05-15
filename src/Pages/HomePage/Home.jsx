9289975e4f7560f97b947f6a915776073d0bd8d7
neetukumari4858 <neetu21@navgurukul.org>
2022-05-16 01:41:48 +0530

    Feat :Added Rules Page

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -3,26 +3,24 @@ import "./Home.css"
 import { Footer } from '../../Components/Footer/Footer'
 // import { Link } from 'react-router-dom'
 import { badminton, cricket, football } from "./../../Image"
+import { Link } from 'react-router-dom'
 const Home = () => {
     return (
         <>
         <div className="outer-container">
             <div className="headings">
                 <h1 className='quizHeading-one'>Quiz Masters</h1>
-                {/* <h2>Let's test your memory</h2>  */}
                 <h2 className='quizHeadingTwo'>Featured Sports Quiz</h2>
             </div>
             <div className="first-section">
-                {/* <!-- first box --> */}
                 <div className="categories">
-                    <img className="images" src={cricket} alt='error' />
-                    <div className="text-div">
+                <Link to="RulesPage"><img className="images" src={cricket} alt='error' /></Link>
+                    <div className="text-div" >
                         <p className="paragraph">You don't Play For the Crowd,you Play For the country!</p>
                         <h1 className="button quizHeading-one">Cricket</h1>
                     </div>
 
                 </div>
-                {/* <!-- second box --> */}
                 <div className="categories">
                     <img className="images" src={football} alt='error' />
                     <div className="text-div">
@@ -31,7 +29,6 @@ const Home = () => {
                     </div>
 
                 </div>
-                {/* <!-- third box--> */}
                 <div className="categories">
                     <img className="images" src={badminton} alt='error' />
                     <div className="text-div">
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,5 +1,6 @@
 import { Routes,Route } from "react-router-dom"
 import { SignUp } from "../Pages/Auth/SignUp.jsx"
+import { Rules } from "../Pages/Rules/Rules.jsx"
 import { Login } from "./../Pages/Auth/Login"
 import {Home } from "./../Pages/HomePage/Home"
 
@@ -9,6 +10,7 @@ const AppRouter=()=>{
             <Route path="/" element={<Home />}/>
             <Route path="/LoginPage" element={<Login />}/>
             <Route path="/SignUpPage" element={<SignUp/>}/>
+            <Route path="/RulesPage" element={<Rules/>}/>
         </Routes>
     )
 }
