9e5cfcb586801530f5b5813c44f8434891bc649f
neetukumari4858 <neetu21@navgurukul.org>
2022-05-19 15:47:59 +0530

    style.:removed comments

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -23,30 +23,7 @@ const Home = () => {
                     />
                     )
             })}
-                {/* <div className="categories"> */}
-                {/* <Link to="RulesPage"><img className="images" src={cricket} alt='error' /></Link>
-                    <div className="text-div" >
-                        <p className="paragraph">You don't Play For the Crowd,you Play For the country!</p>
-                        <h1 className="button quizHeading-one">Cricket</h1>
-                    </div>
 
-                </div>
-                <div className="categories">
-                    <img className="images" src={football} alt='error' />
-                    <div className="text-div">
-                        <p className="paragraph">You don't Play For the Crowd,you Play For the country!</p>
-                        <h1 className="button quizHeading-one">Football</h1>
-                    </div>
-
-                </div>
-                <div className="categories">
-                    <img className="images" src={badminton} alt='error' />
-                    <div className="text-div">
-                        <p className="paragraph">You don't Play For the Crowd,you Play For the country!</p>
-                        <h1 className="button quizHeading-one">Basketball</h1>
-                    </div>
-
-                </div> */}
             </div>
 
         </div>
