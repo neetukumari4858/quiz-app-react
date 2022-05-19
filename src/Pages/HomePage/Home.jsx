63df254481396b9927b6d65b25b2cfc3868fd35e
neetukumari4858 <90403664+neetukumari4858@users.noreply.github.com>
2022-05-19 15:37:53 +0530

    style:removed comment

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -14,41 +14,16 @@ const Home = () => {
             <div className="first-section">
             {quizQuestion.map(({ categoryId, categoryName, categoryImg , categoryDescription}) =>{
                 return (
-                    <CategoryCard
-                        key={categoryId}
-                        categoryId={categoryId}
-                        categoryImg={categoryImg}
-                        categoryName={categoryName}
-                        categoryDescription={categoryDescription}
-                    />
-                    )
+                <CategoryCard
+                    key={categoryId}
+                    categoryId={categoryId}
+                    categoryImg={categoryImg}
+                    categoryName={categoryName}
+                    categoryDescription={categoryDescription}
+                />
+                )
             })}
-                {/* <div className="categories"> */}
-                {/* <Link to="RulesPage"><img className="images" src={cricket} alt='error' /></Link>
-                    <div className="text-div" >
-                        <p className="paragraph">You don't Play For the Crowd,you Play For the country!</p>
-                        <h1 className="button quizHeading-one">Cricket</h1>
-                    </div>
-
-                </div>
-                <div className="categories">
-                    <img className="images" src={football} alt='error' />
-                    <div className="text-div">
-                        <p className="paragraph">You don't Play For the Crowd,you Play For the country!</p>
-                        <h1 className="button quizHeading-one">Football</h1>
-                    </div>
-
-                </div>
-                <div className="categories">
-                    <img className="images" src={badminton} alt='error' />
-                    <div className="text-div">
-                        <p className="paragraph">You don't Play For the Crowd,you Play For the country!</p>
-                        <h1 className="button quizHeading-one">Basketball</h1>
-                    </div>
-
-                </div> */}
             </div>
-
         </div>
         <Footer/>
         </>
