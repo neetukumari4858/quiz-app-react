7ac70ead059f8339fd6fc1adec275f7debbc9c97
neetukumari4858 <90403664+neetukumari4858@users.noreply.github.com>
2022-08-27 18:06:31 +0530

    feat:Added context and reducer type

--- src/Hooks/Context/authContext.tsx
+++ src/Hooks/Context/authContext.tsx
@@ -1,36 +1,15 @@
 import { useContext, createContext, useState, useEffect } from "react";
 import {reactChildren} from "./../../types/common.types"
-// type isLoggedInType={
-  
-//   setLogedIn:React.Dispatch<React.SetStateAction<boolean>>
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import {userDetailType,isLogedInType} from "./../../types/auth.types"
 
 const AuthContext = createContext({ isLogedIn: false });
 const AuthProvider = ({ children }:reactChildren) => {
   const userLoggedIn = localStorage.getItem("user") ? true : false;
-  const [isLogedIn, setLogedIn] = useState(userLoggedIn);
-  const [userDetail, setUserdetail] = useState({ token: "", user: {} });
+  const [isLogedIn, setLogedIn] = useState<isLogedInType>(userLoggedIn);
+  const [userDetail, setUserdetail] = useState<userDetailType>({ token: "", user: {} });
   const token = localStorage.getItem("token");
   const localStorageUser = localStorage.getItem("user");
   const user=localStorageUser? JSON.parse(localStorageUser):null
-
 
   useEffect(() => {
     if (token && user) {
